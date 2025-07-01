4c69d05fbaf178d73e82b4186cececead588435c
Faaustoo <fausto.parada12@gmail.com>
2025-07-01 18:39:18 -0300

    correccion de modelo

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Correr migraciones : php artisan migrate:fresh
-Generar roles y permisos : shield:install
+Generar roles y permisos : php artisan shield:install
 Ejecutar seeder: php artisan db:seed
--- app/Models/Minor.php
+++ app/Models/Minor.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Minor extends Model
 {
@@ -18,8 +18,8 @@ class Minor extends Model
         'relacion',
     ];
 
-    public function user(): BelongsTo
+    public function user():HasOne
     {
-        return $this->belongsTo(User::class);
+        return $this->HasOne(User::class);
     }
 }

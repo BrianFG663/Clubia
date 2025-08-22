0d7b833a9f0ada17f33094fd85c41cf43836207d
brian gonzalez <briangonzaz305@gmail.com>
2025-08-22 16:14:50 -0300

    Permisos paginas perzonalizadas

--- app/Filament/Pages/Facturas.php
+++ app/Filament/Pages/Facturas.php
@@ -4,9 +4,18 @@ namespace App\Filament\Pages;
 
 use App\Models\Institution;
 use Filament\Pages\Page;
+use Illuminate\Support\Facades\Auth;
 
 class Facturas extends Page
 {
+
+    public static function canAccess(): bool{
+        /** @var \App\Models\User|null $user */
+        $user = \Illuminate\Support\Facades\Auth::user();
+
+        return $user?->can('page_Facturas') ?? false;
+    }
+
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static ?string $navigationGroup = '🧾Facturacion';
     protected static ?string $navigationLabel = 'Facturación Mensual';

35668942aab557b8762d8b81e61521a1befa3af9
MercadoLucila <mercadoruizlucila@hotmail.com>
2025-09-02 21:02:41 -0300

    Intento de solucionar lo del renderizado del formulario y el grafico de actividades y su info

--- app/Filament/Widgets/ActividadSubactividadesChart.php
+++ app/Filament/Widgets/ActividadSubactividadesChart.php
@@ -17,51 +17,54 @@ class ActividadSubactividadesChart extends ChartWidget implements HasForms
 
     public ?int $actividadId = null;
 
-    protected function getFormSchema(): array 
+    protected function getFormSchema(): array
     {
         return [
             Forms\Components\Select::make('actividadId')
                 ->label('Selecciona una Actividad')
-                ->options(
-                    Activity::pluck('nombre','id')
-                )
+                ->options(Activity::pluck('nombre', 'id'))
                 ->searchable()
-                ->reactive()
+                ->reactive(),
         ];
     }
 
+    
     protected function getData(): array
     {
-        if (! $this->actividadId) 
-        {
-            return 
-            [
+        if (! $this->actividadId) {
+            return [
                 'datasets' => [],
-                'labels' => [],
+                'labels'   => [],
             ];
         }
 
         $actividad = Activity::with('subActivities.partners')->find($this->actividadId);
-        $labels = $actividad->subActivities->pluck('nombre')->toArray();
-        $data = $actividad->subActivities->map(fn ($sub) => $sub->partners->count())->toArray();
 
+        $labels = $actividad->subActivities->pluck('nombre')->toArray();
+        $data   = $actividad->subActivities->map(fn ($sub) => $sub->partners->count())->toArray();
 
         return [
             'datasets' => [
                 [
                     'label' => 'Cantidad de socios',
-                    'data' => $data,
-                ],                
+                    'data'  => $data,
+                    'backgroundColor' => '#3b82f6', // azul Tailwind
+                ],
             ],
             'labels' => $labels,
         ];
-        
-        
     }
 
     protected function getType(): string
     {
         return 'bar';
+    }
+
+    protected function getContent(): ?\Illuminate\Contracts\View\View
+    {
+        return view('filament.widgets.activity-subactivities-form', [
+            'form' => $this->form,
+        ]);
     }
 
     
--- resources/views/filament/widgets/activity-subactivities-chart.blade.php
+++ resources/views/filament/widgets/activity-subactivities-chart.blade.php
@@ -1,8 +1,5 @@
-<x-filament-widgets::widget>
-    <x-filament::card>
-        <div class="mb-4">
-            {{ $this->form }}
-        </div>
-    </x-filament::card>
 
-</x-filament-widgets::widget>
+        <div class="mb-4">
+            {{ $form }}
+        </div>
+

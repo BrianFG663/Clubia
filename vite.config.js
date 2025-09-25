fd7f3760da6dbb04bc90f7cc7299351235509690
brian gonzalez <briangonzaz305@gmail.com>
2025-09-25 15:23:55 -0300

    Mi primer commit en mi-rama

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        https: true, // solo si tenés certificado local
+    },
     plugins: [
         laravel({
             input: [

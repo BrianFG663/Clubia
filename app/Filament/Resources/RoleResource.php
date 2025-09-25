e9644dce546c56886b5a76e4f3810550b6d6cc57
brian gonzalez <briangonzaz305@gmail.com>
2025-09-25 13:45:40 -0300

    Mi primer commit en mi-rama

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -10,6 +10,7 @@ use BezhanSalleh\FilamentShield\Traits\HasShieldFormComponents;
 use Filament\Facades\Filament;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Spatie\Permission\Models\Permission;
 use Filament\Pages\SubNavigationPosition;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -43,53 +44,39 @@ class RoleResource extends Resource implements HasShieldPermissions
     }
 
     public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\Grid::make()
-                    ->schema([
-                        Forms\Components\Section::make()
-                            ->schema([
-                                Forms\Components\TextInput::make('name')
-                                    ->label(__('filament-shield::filament-shield.field.name'))
-                                    ->unique(
-                                        ignoreRecord: true,
-                                        /** @phpstan-ignore-next-line */
-                                        modifyRuleUsing: fn(Unique $rule) => Utils::isTenancyEnabled() ? $rule->where(Utils::getTenantModelForeignKey(), Filament::getTenant()?->id) : $rule
-                                    )
-                                    ->required()
-                                    ->maxLength(255),
+{
+    $customPermissions = Permission::whereIn('name', [
+        'access_admin_panel',
+    ])->pluck('name', 'name');
 
-                                Forms\Components\TextInput::make('guard_name')
-                                    ->label(__('filament-shield::filament-shield.field.guard_name'))
-                                    ->default(Utils::getFilamentAuthGuard())
-                                    ->nullable()
-                                    ->maxLength(255),
+    return $form
+        ->schema([
+            Forms\Components\Grid::make()
+                ->schema([
+                    Forms\Components\Section::make()
+                        ->schema([
+                            Forms\Components\TextInput::make('name')
+                                ->label('Nombre del rol')
+                                ->required()
+                                ->maxLength(255),
 
-                                Forms\Components\Select::make(config('permission.column_names.team_foreign_key'))
-                                    ->label(__('filament-shield::filament-shield.field.team'))
-                                    ->placeholder(__('filament-shield::filament-shield.field.team.placeholder'))
-                                    /** @phpstan-ignore-next-line */
-                                    ->default([Filament::getTenant()?->id])
-                                    ->options(fn(): Arrayable => Utils::getTenantModel() ? Utils::getTenantModel()::pluck('name', 'id') : collect())
-                                    ->hidden(fn(): bool => ! (static::shield()->isCentralApp() && Utils::isTenancyEnabled()))
-                                    ->dehydrated(fn(): bool => ! (static::shield()->isCentralApp() && Utils::isTenancyEnabled())),
-                                ShieldSelectAllToggle::make('select_all')
-                                    ->onIcon('heroicon-s-shield-check')
-                                    ->offIcon('heroicon-s-shield-exclamation')
-                                    ->label(__('filament-shield::filament-shield.field.select_all.name'))
-                                    ->helperText(fn(): HtmlString => new HtmlString(__('filament-shield::filament-shield.field.select_all.message')))
-                                    ->dehydrated(fn(bool $state): bool => $state),
-
-                            ])
-                            ->columns([
-                                'sm' => 2,
-                                'lg' => 3,
-                            ]),
-                    ]),
-                static::getShieldFormComponents(),
-            ]);
-    }
+                            Forms\Components\TextInput::make('guard_name')
+                                ->label('Guard')
+                                ->default('web')
+                                ->maxLength(255),
+                        ])
+                        ->columns([
+                            'sm' => 2,
+                            'lg' => 3,
+                        ]),
+                ]),
+            static::getShieldFormComponents(),
+            Forms\Components\CheckboxList::make('permissions')
+                ->label('Permisos personalizados')
+                ->options($customPermissions)
+                ->columns(2),
+        ]);
+}
 
     public static function table(Table $table): Table
     {

0890ae233e06c35e7afbdac4d1afbfcd870d314d
brian gonzalez <briangonzaz305@gmail.com>
2025-11-01 00:50:02 -0300

    Version CCE listo para presentacion

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     server: {
-        https: false, // solo si tenés certificado local
+        https: true, // solo si tenés certificado local
     },
     plugins: [
         laravel({
